436d14b4117ae0767858cd5274c1c44a10b6924a
Filip Sosnowski <01169626@pw.edu.pl>
2022-10-18 16:03:39 +0200

    Dodanie metody pop i put do Heap

---
+++ aisd_lab_1_sorting/src/main/java/pl/edu/pw/ee/Heap.java
@@ -0,0 +1,43 @@
+package pl.edu.pw.ee;
+
+import java.util.List;
+import pl.edu.pw.ee.services.HeapExtension;
+import pl.edu.pw.ee.services.HeapInterface;
+
+public class Heap<T extends Comparable<T>> implements HeapInterface<T>, HeapExtension {
+
+    private List<T> data;
+
+    public Heap(List<T> data) {
+        this.data = data;
+    }
+
+    @Override
+    public void put(T item) {
+        this.data.add(item);
+    }
+
+    @Override
+    public T pop() {
+        T elementToBePopped = null;
+        if (!this.data.isEmpty()) {
+            elementToBePopped = this.data.remove(0);
+        }
+        return elementToBePopped;
+    }
+
+    @Override
+    public void build() {
+        int n = data.size();
+
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(i, n);
+        }
+    }
+
+    @Override
+    public void heapify(int startId, int endId) {
+        // TODO
+    }
+
+}
